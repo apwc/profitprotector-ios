953799bd25d0cf17c617dd1e24607ad412379cb0
Davide Scheriani <info@digitalvanilla.com>
2015-07-08 12:41:37 +0700

    first commit

---
+++ Profit Protector/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Profit Protector
+//
+//  Created by Davide Scheriani on 7/8/15.
+//  Copyright (c) 2015 CleanBrands LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Profit Protector/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Profit Protector
+//
+//  Created by Davide Scheriani on 7/8/15.
+//  Copyright (c) 2015 CleanBrands LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
