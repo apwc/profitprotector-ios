44275379ba8184ce60fd6c11f3fc5074a6049613
Davide Scheriani <info@digitalvanilla.com>
2015-07-11 11:28:48 +0700

    updates

--- Profit Protector/Classes/SignupViewController.m
+++ Profit Protector/Classes/SignupViewController.m
@@ -37,25 +37,110 @@
   CGFloat buttonHeight = 60.0f;
   
   // username
-  UITextField *username = [[UITextField alloc] initWithFrame:CGRectMake(60.0f,
-                                                                        290.0f,
-                                                                        CGRectGetWidth(self.view.bounds) - 60.0f,
-                                                                        textFieldHeight)];
-  username.delegate = self;
-  username.font = [UIFont fontWithName:@"HelveticaNeue" size:textFieldFontsize];
-  username.textColor = [UIColor blackColor];
-  username.placeholder = @"<username>";
-  username.autocorrectionType = UITextAutocorrectionTypeNo;
-  username.autocapitalizationType = UITextAutocapitalizationTypeNone;
-  [self.view addSubview:username];
+  UITextField *name = [[UITextField alloc] initWithFrame:CGRectMake(60.0f,
+                                                                    130.0f,
+                                                                    CGRectGetWidth(self.view.bounds) - 60.0f,
+                                                                    textFieldHeight)];
+  name.delegate = self;
+  name.font = [UIFont fontWithName:@"HelveticaNeue" size:textFieldFontsize];
+  name.textColor = [UIColor blackColor];
+  name.placeholder = @"<name>";
+  name.autocorrectionType = UITextAutocorrectionTypeNo;
+  name.autocapitalizationType = UITextAutocapitalizationTypeNone;
+  [self.view addSubview:name];
   
   // division line
   UIView *divisionLine1 = [[UIView alloc] initWithFrame:CGRectMake(0.0f,
-                                                                   290.0f + textFieldHeight,
+                                                                   130.0f + textFieldHeight,
                                                                    CGRectGetWidth(self.view.bounds),
                                                                    1.0f)];
   divisionLine1.backgroundColor = [UIColor colorWithWhite:0.7f alpha:1.0f];
   [self.view addSubview:divisionLine1];
+  
+  // username
+  UITextField *email = [[UITextField alloc] initWithFrame:CGRectMake(60.0f,
+                                                                     170.0f,
+                                                                     CGRectGetWidth(self.view.bounds) - 60.0f,
+                                                                     textFieldHeight)];
+  email.delegate = self;
+  email.font = [UIFont fontWithName:@"HelveticaNeue" size:textFieldFontsize];
+  email.textColor = [UIColor blackColor];
+  email.placeholder = @"<email>";
+  email.autocorrectionType = UITextAutocorrectionTypeNo;
+  email.autocapitalizationType = UITextAutocapitalizationTypeNone;
+  [self.view addSubview:email];
+  
+  // division line
+  UIView *divisionLine2 = [[UIView alloc] initWithFrame:CGRectMake(0.0f,
+                                                                   170.0f + textFieldHeight,
+                                                                   CGRectGetWidth(self.view.bounds),
+                                                                   1.0f)];
+  divisionLine2.backgroundColor = [UIColor colorWithWhite:0.7f alpha:1.0f];
+  [self.view addSubview:divisionLine2];
+  
+  // username
+  UITextField *password = [[UITextField alloc] initWithFrame:CGRectMake(60.0f,
+                                                                        210.0f,
+                                                                        CGRectGetWidth(self.view.bounds) - 60.0f,
+                                                                        textFieldHeight)];
+  password.delegate = self;
+  password.font = [UIFont fontWithName:@"HelveticaNeue" size:textFieldFontsize];
+  password.textColor = [UIColor blackColor];
+  password.placeholder = @"<password>";
+  password.autocorrectionType = UITextAutocorrectionTypeNo;
+  password.autocapitalizationType = UITextAutocapitalizationTypeNone;
+  [self.view addSubview:password];
+  
+  // division line
+  UIView *divisionLine3 = [[UIView alloc] initWithFrame:CGRectMake(0.0f,
+                                                                   210.0f + textFieldHeight,
+                                                                   CGRectGetWidth(self.view.bounds),
+                                                                   1.0f)];
+  divisionLine3.backgroundColor = [UIColor colorWithWhite:0.7f alpha:1.0f];
+  [self.view addSubview:divisionLine3];
+  
+  // username
+  UITextField *company = [[UITextField alloc] initWithFrame:CGRectMake(60.0f,
+                                                                       250.0f,
+                                                                       CGRectGetWidth(self.view.bounds) - 60.0f,
+                                                                       textFieldHeight)];
+  company.delegate = self;
+  company.font = [UIFont fontWithName:@"HelveticaNeue" size:textFieldFontsize];
+  company.textColor = [UIColor blackColor];
+  company.placeholder = @"<company>";
+  company.autocorrectionType = UITextAutocorrectionTypeNo;
+  company.autocapitalizationType = UITextAutocapitalizationTypeNone;
+  [self.view addSubview:company];
+  
+  // division line
+  UIView *divisionLine4 = [[UIView alloc] initWithFrame:CGRectMake(0.0f,
+                                                                   250.0f + textFieldHeight,
+                                                                   CGRectGetWidth(self.view.bounds),
+                                                                   1.0f)];
+  divisionLine4.backgroundColor = [UIColor colorWithWhite:0.7f alpha:1.0f];
+  [self.view addSubview:divisionLine4];
+  
+  // username
+  UITextField *type = [[UITextField alloc] initWithFrame:CGRectMake(60.0f,
+                                                                    290.0f,
+                                                                    CGRectGetWidth(self.view.bounds) - 60.0f,
+                                                                    textFieldHeight)];
+  type.delegate = self;
+  type.font = [UIFont fontWithName:@"HelveticaNeue" size:textFieldFontsize];
+  type.textColor = [UIColor blackColor];
+  type.placeholder = @"<type>";
+  type.autocorrectionType = UITextAutocorrectionTypeNo;
+  type.autocapitalizationType = UITextAutocapitalizationTypeNone;
+  type.enabled = NO;
+  [self.view addSubview:type];
+  
+  // division line
+  UIView *divisionLine5 = [[UIView alloc] initWithFrame:CGRectMake(0.0f,
+                                                                   290.0f + textFieldHeight,
+                                                                   CGRectGetWidth(self.view.bounds),
+                                                                   1.0f)];
+  divisionLine5.backgroundColor = [UIColor colorWithWhite:0.7f alpha:1.0f];
+  [self.view addSubview:divisionLine5];
   
   // signup
   UIButton *signup = [[UIButton alloc] initWithFrame:CGRectMake(0.0f,
