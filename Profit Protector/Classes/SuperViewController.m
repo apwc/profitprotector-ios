5908a8eccff8fec621ce34759926902afb4a82f2
Davide Scheriani <info@digitalvanilla.com>
2015-07-11 18:51:08 +0700

    updates

--- Profit Protector/Classes/SuperViewController.m
+++ Profit Protector/Classes/SuperViewController.m
@@ -10,7 +10,6 @@
   [super viewDidLoad];
   
   // UI customizations
-  //self.view.backgroundColor = [UIColor lightGrayColor];
   UIImageView *background = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"bkg"]];
   background.contentMode = UIViewContentModeScaleAspectFill;
   [self.view addSubview:background];
